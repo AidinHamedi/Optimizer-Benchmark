2e3cc6e7992ae1a320dde5dbed9f51d9b69b7c26
AidinHamedi <aidin.hamediasl@gmail.com>
2025-08-13 21:46:23 +0330

    add: basic entry point

--- benchmark/evaluate.py
+++ benchmark/evaluate.py
@@ -1,12 +1,32 @@
+import os
+import shutil
+from pathlib import Path
 from typing import Callable
 
 import torch
 from torch import nn
 
-from . import functions
+from .functions import FUNC_DICT
 from .utils.executor import execute_steps
 from .utils.model import Pos2D
 
 
-def benchmark_optimizer(optimizer_maker: Callable, optimizer_name: str):
-    pass
+def benchmark_optimizer(
+    optimizer_maker: Callable,
+    optimizer_name: str,
+    output_dir: Path,
+    hypr_search_spaces: dict,
+    config: dict,
+):
+    results_dir = os.path.join(output_dir, optimizer_name)
+
+    if os.path.exists(results_dir):
+        shutil.rmtree(results_dir)
+
+    os.makedirs(results_dir)
+
+    for name, data in FUNC_DICT.items():
+        func = data["func"]
+        eval_size = data["size"]
+        start_pos = data["pos"]
+        print(name, eval_size, start_pos)
--- runner.py
+++ runner.py
@@ -1,5 +1,53 @@
+import tomllib
+from pathlib import Path
+
+from pytorch_optimizer import create_optimizer, get_supported_optimizers
+
+from benchmark.evaluate import benchmark_optimizer
+
+CONFIG_DIR = Path("./config.toml")
+OUTPUT_DIR = Path("./results")
+
+
+def read_toml_config(path: Path) -> dict:
+    """Read and parse a TOML configuration file into a dictionary."""
+    file_path = Path(path)
+    if not file_path.is_file():
+        raise FileNotFoundError(f"Config file not found: {path}")
+    with file_path.open("rb") as f:
+        return tomllib.load(f)
+
+
+def get_hypr_search_space(name: str, config: dict) -> dict:
+    """Return the hyperparameter search space for a given optimizer name."""
+    return config.get(name, config["default"])
+
+
 def main():
-    print("Hello from ml-optimizer-benchmark!")
+    """The entry point of the app."""
+    configs = read_toml_config(CONFIG_DIR)
+    optimizers = get_supported_optimizers()
+
+    for i, optimizer_name in enumerate(optimizers, start=1):
+        search_space = get_hypr_search_space(
+            optimizer_name, configs["HyprSearchSpaces"]
+        )
+        print(
+            f"({i}/{len(optimizers)}) Processing {optimizer_name}... (Params to tune: {', '.join(search_space.keys())})"
+        )
+
+        def get_optimizer(model, optimizer_config):
+            return create_optimizer(model, optimizer_name, **optimizer_config)
+
+        benchmark_optimizer(
+            get_optimizer,
+            optimizer_name,
+            OUTPUT_DIR,
+            search_space,
+            configs["EvalConfigs"],
+        )
+
+    print("Done!")
 
 
 if __name__ == "__main__":
