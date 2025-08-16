ba2a9cf376d58b3f540da815ea7eac50ed05b27b
AidinHamedi <aidin.hamediasl@gmail.com>
2025-08-16 23:28:32 +0330

    update: smaller eggholder function

--- benchmark/functions/eggholder.py
+++ benchmark/functions/eggholder.py
@@ -2,9 +2,15 @@ import torch
 
 from .norm import normalize
 
-START_POS = torch.tensor([47.59, 48.59])
-EVAL_SIZE = ((-512, 512), (-512, 512))
-GLOBAL_MINIMUM_LOC = torch.tensor([[512.0, 404.2319]])
+START_POS = torch.tensor([0.9294921875, 0.9490234375])
+EVAL_SIZE = ((-10, 10), (-10, 10))
+GLOBAL_MINIMUM_LOC = torch.tensor(
+    [
+        [10.00000000000, 7.89515429688],
+        [10.40000000000, -11.92380900000],
+        [-11.54285700000, -12.00000000000],
+    ]
+)
 
 
 @normalize(-1092.7841, 1199.1733)
@@ -20,8 +26,9 @@ def eggholder(x: torch.Tensor) -> torch.Tensor:
         torch.Tensor: A scalar tensor representing the value of the Eggholder function
             for the given input 'x'.
     """
-    x1 = x[0]
-    x2 = x[1]
+    # To make the eval size smaller
+    x1 = x[0] * 51.2
+    x2 = x[1] * 51.2
 
     term1 = -(x2 + 47) * torch.sin(torch.sqrt(torch.abs(x2 + x1 / 2 + 47)))
     term2 = -x1 * torch.sin(torch.sqrt(torch.abs(x1 - (x2 + 47))))
