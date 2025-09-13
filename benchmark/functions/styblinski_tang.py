3fa88ebf4b66cd72765ac5ac7f9d3060a082d270
AidinHamedi <aidin.hamediasl@gmail.com>
2025-09-13 10:46:12 +0330

    update: Styblinski-Tang start pos

--- benchmark/functions/styblinski_tang.py
+++ benchmark/functions/styblinski_tang.py
@@ -2,7 +2,7 @@ import torch
 
 from .norm import normalize
 
-START_POS = torch.tensor([4.45, 0.13])
+START_POS = torch.tensor([4.45, 4.1])
 EVAL_SIZE = ((-5, 5), (-5, 5))
 GLOBAL_MINIMUM_LOC = torch.tensor([[-2.903534, -2.903534]])
 
