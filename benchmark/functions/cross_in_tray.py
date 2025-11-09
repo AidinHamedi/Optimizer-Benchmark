988577d72b45e96456e2538fa59e7b49790eed89
AidinHamedi <aidin.hamediasl@gmail.com>
2025-11-09 19:21:59 +0330

    update: "Cross-in-Tray" func scale

--- benchmark/functions/cross_in_tray.py
+++ benchmark/functions/cross_in_tray.py
@@ -3,26 +3,38 @@ import torch
 from .norm import normalize
 
 FUNCTION_NAME = "Cross-in-Tray"
-START_POS = torch.tensor([16.6, 21.3])
-EVAL_SIZE = ((-22, 22), (-22, 22))
+START_POS = torch.tensor([6.6, 8.3])
+EVAL_SIZE = ((-10, 10), (-10, 10))
+
+FUNC_SCALE = 5
+
 GLOBAL_MINIMUM_LOC = torch.tensor(
-    [[1.3491, 1.3491], [-1.3491, 1.3491], [1.3491, -1.3491], [-1.3491, -1.3491]]
+    [
+        [-0.26586073637008667, -0.26586073637008667],
+        [-0.26586073637008667, 0.26586073637008667],
+        [0.26586073637008667, -0.26586073637008667],
+        [0.26586073637008667, 0.26586073637008667],
+    ]
 )
 
 
-@normalize(-20625.36328125, -1015.8145751953125)
+@normalize(-20625.240234375, -806.4556884765625)
 @torch.jit.script
-def cross_in_tray(x: torch.Tensor) -> torch.Tensor:
+def cross_in_tray(
+    x: torch.Tensor,
+    scale: float = FUNC_SCALE,
+) -> torch.Tensor:
     """Computes the Cross-in-tray function.
 
     Args:
         x (torch.Tensor): A tensor with last dimension size 2, representing [x, y].
+        scale (torch.Tensor): Scale factor for [x, y].
 
     Returns:
         torch.Tensor: Scalar tensor with the Cross-in-tray function value.
     """
-    x1 = x[..., 0].type(torch.float64, non_blocking=True)
-    x2 = x[..., 1].type(torch.float64, non_blocking=True)
+    x1 = x[..., 0].type(torch.float64, non_blocking=True) * scale
+    x2 = x[..., 1].type(torch.float64, non_blocking=True) * scale
 
     fact1 = torch.sin(x1) * torch.sin(x2)
     r = torch.sqrt(x1 * x1 + x2 * x2)
